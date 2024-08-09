92d171e336100b284ab481f2c927ddd15fc8acf9
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-08-09 15:59:39 +0530

    Review

--- feedback/src/types/ApiResponse.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from "@/Model/User";
-export interface ApiResponse{
-    success: boolean;
-    message: string;
-    isAccepingMessages? : boolean;
-    messages?: Array<Message>
-
-}
